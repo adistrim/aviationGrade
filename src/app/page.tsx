fdc1dcd2ca4c8b7b33439b1e4cd43e4154e6618d
adistrim <adistrim.dev@gmail.com>
2023-12-19 11:58:55 +0530

    added scroll function after page change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Header from './components/Header';
 import SearchBar from './components/SearchBar';
 import AircraftList from './components/AircraftList';
@@ -113,6 +113,14 @@ const Home = () => {
   const cardsPerPage = 10;
   const [currentPage, setCurrentPage] = useState(1);
   const totalPages = Math.ceil(aircraftData.length / cardsPerPage);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    // Smooth scroll to the top of the container when currentPage changes
+    if (containerRef.current) {
+      containerRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, [currentPage]);
 
   const updatePagination = (newPage: number) => {
     setCurrentPage(newPage);
@@ -123,7 +131,7 @@ const Home = () => {
   const visibleAircraftData = aircraftData.slice(start, end);
   
   return (
-    <div className="font-serif">
+    <div className="font-serif" ref={containerRef}>
       <Header />
       <div className="max-w-[1000px] bg-white shadow-[0_0_10px_rgba(0,0,0,0.1)] mx-auto my-5 p-5 rounded-[5px]">
         <SearchBar />
