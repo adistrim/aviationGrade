c9cb3f3a6945b6f9343756463cb912160e5e8b31
adistrim <adistrim.dev@gmail.com>
2024-01-02 18:50:06 +0530

    Footer content changed

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer: React.FC = () => {
     return (
         <div className="bg-[#333] text-xs md:text-sm xl:text-base text-white text-center px-0 py-6">
-            <p>&copy; 2023 adistrim. All rights reserved. | Created by adistrim &ndash; <a className="text-white underline"
+            <p>Created by adistrim &ndash; <a className="text-white underline"
                 href="https://github.com/adistrim/aviationGrade" target="_blank">View Source Code</a></p>
         </div>
     )
